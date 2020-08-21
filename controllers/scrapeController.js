e48ebda508765e630b6a2bdb34391f744b400463
ericsmith92 <ericsmth884@gmail.com>
2020-08-21 09:07:23 -0400

    committing progress on this branch before refactoring

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -3,6 +3,7 @@ const Account = mongoose.model('Account');
 const axios = require('axios');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
+const moment = require('moment');
 
 exports.homePage = (req, res) => {
     res.render('index');
@@ -31,5 +32,34 @@ exports.scrapeAccount = async (req, res, next) => {
 
 exports.saveAccount = async (req, res) => {
     const account = await (new Account(req.body)).save();
-    res.redirect('/');
+    res.render('preview', { account });
+}
+
+exports.checkAccount = async (req, res, next) => {   
+    const account = await Account.findOne( { handle:  req.body.search } );
+    
+    //if no account exists, lets pass to scrape
+    if(!account){
+        next();
+        return;
+    }
+
+    //otherwise, lets check when we last scraped it, see if it's been longer than an hour
+    const lastScraped = account.created.getTime();
+    const diff = Date.now() - lastScraped;
+    const millisecsHour = 3600000;
+
+    //if less than an hour ago, head directly to preview
+    if(diff <= millisecsHour){
+        console.log('scraped under an hour ago', millisecsHour - diff);
+        res.render('preview', { account });
+    }else{
+    //otherwise, it exists but needs to be scraped again, send to update
+        //res.json({diff});
+        next('/update');
+    }
+}
+
+exports.update = (req, res) => {
+    res.json({'it': 'worked'});
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const scrapeController = require('../controllers/scrapeController');
 
 router.get('/', scrapeController.homePage);
-router.post('/scrape', scrapeController.scrapeAccount, 
+router.post('/scrape', scrapeController.checkAccount, 
+                       scrapeController.scrapeAccount, 
                        scrapeController.saveAccount);
+router.get('/update', scrapeController.update);
 
 module.exports = router;
